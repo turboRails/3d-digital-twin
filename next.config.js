18e6a93d9c34b671ffdaa5a47514481a5507ab7a
Upendra N <n.upendra@gvatech.co.jp>
2023-11-08 01:16:52 +0900

    First ＊対応

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const runtimeCaching = require("next-pwa/cache");
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  runtimeCaching,
+  disable: process.env.NODE_ENV === 'development',
+});
 
-module.exports = nextConfig
+const nextConfig = withPWA({
+  eslint: {
+    dirs: ['src'],
+    ignoreDuringBuilds: true,
+  },
+});
+module.exports = nextConfig;
