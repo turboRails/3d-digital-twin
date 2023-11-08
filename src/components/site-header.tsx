57a0b5fea584809715054ce1e254d7c32747f056
Upendra N <n.upendra@gvatech.co.jp>
2023-11-08 18:57:08 +0900

    remove twitter

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -27,15 +27,6 @@ export function SiteHeader() {
                 <Icons.gitHub className="h-5 w-5" />
                 <span className="sr-only">GitHub</span>
               </div>
-              <div
-                className={buttonVariants({
-                  size: "icon",
-                  variant: "ghost",
-                })}
-              >
-                <Icons.twitter className="h-5 w-5 fill-current" />
-                <span className="sr-only">Twitter</span>
-              </div>
             </Link>
             <ThemeToggle />
           </nav>
