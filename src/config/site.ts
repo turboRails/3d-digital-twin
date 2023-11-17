e49050603fe588d902e5fe7eb84740e6aadee9f3
Nallapa Upendra <turboRails@users.noreply.github.com>
2023-11-17 11:41:11 +0900

    Update site.ts

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,9 +1,9 @@
 export type SiteConfig = typeof siteConfig
 
 export const siteConfig = {
-  name: "Next.js",
+  name: "Future Monitoring",
   description:
-    "Beautifully designed components built with Radix UI and Tailwind CSS.",
+    "Beautifully designed 3D digital twins for monitoring",
   mainNav: [
     {
       title: "Home",
@@ -11,8 +11,7 @@ export const siteConfig = {
     },
   ],
   links: {
-    twitter: "https://twitter.com/shadcn",
-    github: "https://github.com/shadcn/ui",
-    docs: "https://ui.shadcn.com",
+    Energy: "/energy",
+    Heat: "/heat",
   },
 }
